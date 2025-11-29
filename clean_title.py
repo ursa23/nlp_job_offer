652d7c1643024de0c8c7c193d6463b5f7511daf2
ursa23 <randriarivelo.ursula@gmail.com>
2025-11-29 04:48:35 +0300

    update clean_title.py

--- clean_title.py
+++ clean_title.py
@@ -1,18 +1,5 @@
-import pandas as pd
 import re
-from sqlalchemy import create_engine
-
-
-
-DATABASE_URI = 'postgresql://root:0000@192.168.1.120:5432/test'
-QUERY = "SELECT mission_clean, profil_clean, title_clean FROM test_schema.portaljob_test ORDER BY title_clean DESC LIMIT 1"
-
-engine = create_engine(DATABASE_URI)
-try:
-    df = pd.read_sql_query(QUERY, engine)
-    print(f"✅ {len(df)} lignes récupérées de la base de données.")
-except Exception as e:
-    raise Exception(f"❌ Erreur lors de la requête SQL : {e}")
+from database_connection import df
 
 # Nettoyage de base
 df.dropna(subset=['title_clean'], inplace=True)
@@ -23,10 +10,12 @@ def clean_title(title):
     if not isinstance(title, str):
         return ""
     title = title.lower()
+    title = re.sub(r"[’'‘\"]", " ", title)
     title = re.sub(r'[^a-z\s]', ' ', title)  # Supprime ponctuation, chiffres, etc.
     title = re.sub(r'\s+', ' ', title).strip()
     return title
-df = df['title_clean']
+df['clean_title'] = df['title_clean'].apply(clean_title)
+df = df[df['clean_title'] != ""]
 
 if __name__ == "__main__":
-    print(df.to_string(index= False))
+    print(df['clean_title'].to_string(index= False))
